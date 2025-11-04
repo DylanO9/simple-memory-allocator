43acb9ac379875abc6e8ec1be6ecc4552d4999b0
DylanO9 <dylan.olivares11@gmail.com>
2025-11-04 04:39:57 +0000

    setup: create header struct that will hold each block allocated to our free list

--- main.c
+++ main.c
@@ -2,11 +2,20 @@
 
 void *my_malloc(size_t);
 
+typedef long Align; // long is the most restrictive type. We want to make sure we are always aligning on memory, therefore, we use the most restrictive type
+union header {
+    struct {
+        union header *ptr;
+        unsigned size; 
+    } s;
+    Align x;
+};
+
 int main() {
 
 }
 
 
 void *my_malloc(size_t n) {
-
+    
 }
