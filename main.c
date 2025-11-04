c023f4f0124c96f1434494210071e69cbe731415
DylanO9 <dylan.olivares11@gmail.com>
2025-11-04 10:13:02 +0000

    fix: cleanup comments from main.c

--- main.c
+++ main.c
@@ -1,23 +1,22 @@
-// Design: First-fit allocation
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#define NALLOC  1024 /* minimum #units to request */
+#define NALLOC  1024
 
-typedef long Align; // long is the most restrictive type. We want to make sure we are always aligning on memory, therefore, we use the most restrictive type
+typedef long Align;
 union header {
     struct {
-        union header *ptr; // Pointer to the next free block in the list
+        union header *ptr;
         unsigned size; 
     } s;
     Align x;
 };
 
 typedef union header Header;
-static Header base; // Start with an empty list
-static Header *freep = NULL; // Start of the free list
+static Header base; 
+static Header *freep = NULL;
 
 void *my_malloc(size_t);
 static Header *morecore(unsigned);
@@ -35,32 +34,25 @@ void *my_malloc(size_t n) {
     Header *p, *prevp;
     unsigned nunits;
     
-    // Calculate how much memory is needed/the minumum size that we are looking for
-        // What variables do we need to calculate or store this? nunits -> unsigned integer -> Why? -> memory shouldn't be 0 or a negative number
     nunits = (n + sizeof(Header) - 1) / sizeof(Header) + 1;
 
-    // What if freep hasn't been allocated yet? initialize base, and assign it it's own address to itself
     if ((prevp = freep) == NULL) {
         base.s.ptr = freep = prevp = &base;
         base.s.size = 0;
     }
             
-    // How are we going to find the free block, and what information, state, objects do we need?
-        // Once we find the free block, we want to remove it, therefore, we need a way to know what our previous block was -> use a prevp
-        // We need an iterator p to move through the blocks
     for (p = prevp->s.ptr; ; prevp = p, p = p->s.ptr) {
-        // As we are iterating what are we looking for? We want to check if our block has a size greater than or equal to units
         if (p->s.size >= nunits) {
             if (p->s.size == nunits) {
                 prevp->s.ptr = p->s.ptr;
             }
-            else { // Remove the tail portion that is not needed
+            else { 
                 p->s.size -= nunits;
                 p += p->s.size;
                 p->s.size = nunits;
             }
-            freep = prevp; // Future searches will begin from where we left off increasing spatial locality and avoid repeated scans from the start
-            return (void *)(p + 1); // (p + 1), so we are pointing to the non-header based memory
+            freep = prevp; 
+            return (void *)(p + 1); 
         } 
         if (p == freep)
             if ((p = morecore(nunits)) == NULL)
@@ -69,22 +61,13 @@ void *my_malloc(size_t n) {
 }
 
 void my_free(void *ap) {
-    // Okay, so we are given the pointer to free
-    // This pointer is the memory that we gave them, so one sizeof(Header) backwards means that is where our first header lies
-
-    // When we free what are we doing? We are adding the block back to the free list that we have
-    // The memory inside of this is potentially already been messed up with garbage inside
+    Header *ptr = (Header *)ap - 1; 
     
-    // Our goal of this is to take the memory, potentially clear it, then place the memory back into our list
-    Header *ptr = (Header *)ap - 1; // Cast ap to a Header, so subtracting one subtracts the size of one Header element    
-    
-    // We want to place the block, so that the list stays in an order of growing addresses. We can compare addresses by just using the value of the pointers
     Header *p, *prevp;
     for (p = freep; !(p < ptr && ptr < ptr->s.ptr); p = p->s.ptr)
         if (p >= p->s.ptr && (p > ptr || ptr > p->s.ptr ))
             break;
-    // Now we have the spot to insert
-    // Coalesce if the addresses are adjacent
+
     if (ptr + ptr->s.size == p->s.ptr) {
         ptr->s.size += p->s.ptr->s.size;
         ptr->s.ptr = p->s.ptr->s.ptr; 
@@ -99,16 +82,13 @@ void my_free(void *ap) {
 }
 
 static Header *morecore(unsigned nu) {
-    // What do we do in morecore?
-    // Obtain storage from the operating system
-        // use sbrk(total)
-    char *cp; // This is our char pointer
+    char *cp;
     Header *up;
     
-    if (nu < NALLOC) // We need nu to be at least size of nalloc
+    if (nu < NALLOC)
         nu = NALLOC;
     
-    cp = sbrk(nu * sizeof(Header)); // Want nu * sizeof(Header) because..
+    cp = sbrk(nu * sizeof(Header)); 
     if (cp == (char *) -1)
         return NULL;
     up = (Header *) cp;
